88bab1c290a040b6e73b8581df9c09acdcc919f6
Bharti Jangir <142597742+jollyhub8278@users.noreply.github.com>
2025-05-18 16:03:15 +0530

    ejs basics

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const path = require("path");
 const urlRoute = require('./routes/url');
 const {connectToMongoDB} = require('./connect');
 const URL = require('./models/url');
+const staticRoute = require('./routes/staticRouter');
 const app = express();
 const PORT = 8001;
 
@@ -10,7 +12,12 @@ app.use(express.json());
 connectToMongoDB("mongodb://localhost:27017/url-shortner")
 .then(()=>console.log("MongoDb connected!"));
 
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./views"));
+
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
+
 
 app.get('/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
